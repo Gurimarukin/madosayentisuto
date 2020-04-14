a9bb32b08b7713429da1a4ff899e23965e5d7d45
Gurimarukin <naxel@laposte.net>
2020-04-14 14:35:03 +0200

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,31 +11,22 @@ module.exports = {
   ],
   reportUnusedDisableDirectives: true,
   rules: {
-    '@typescript-eslint/array-type': [
-      'error',
-      {
-        default: 'array',
-        readonly: 'array'
-      }
-    ],
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/array-type': ['warn', { default: 'array', readonly: 'array' }],
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_'
-      }
+      'warn',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
     ],
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
-    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-body-style': ['warn', 'as-needed'],
     'array-callback-return': 'off',
     'comma-dangle': [
-      'error',
+      'warn',
       {
         arrays: 'never',
         objects: 'never',
@@ -44,30 +35,19 @@ module.exports = {
         functions: 'never'
       }
     ],
-    'max-len': [
-      'error',
-      {
-        code: 100,
-        tabWidth: 2
-      }
-    ],
+    'max-len': ['warn', { code: 100, tabWidth: 2, ignoreStrings: true }],
     'no-console': 'off',
     'no-empty-function': 'off',
     'no-inner-declarations': 'off',
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1
-      }
-    ],
-    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+    'no-multi-spaces': 'warn',
     'no-redeclare': 'off',
     'no-shadow': 'off',
     'no-undef': 'off',
     'prettier/prettier': 'off',
-    quotes: ['error', 'single'],
+    quotes: ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     'sort-imports': 'off',
-    'space-in-parens': ['error', 'never'],
-    strict: 'error'
+    'space-in-parens': ['warn', 'never'],
+    strict: 'warn'
   }
 }
