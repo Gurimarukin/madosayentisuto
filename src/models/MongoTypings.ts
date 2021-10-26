de8ec7dbba95587b1a26dc2f8de801a0d2b3df46
Gurimarukin <naxel@laposte.net>
2021-10-26 22:27:45 +0200

    Remove unused

--- src/models/MongoTypings.ts
+++ src/models/MongoTypings.ts
@@ -5,10 +5,6 @@ export type WithoutProjection<T> = T & {
   readonly projection?: undefined
 }
 
-export type ReadonlyPartial<T> = {
-  readonly [K in keyof T]?: T[K]
-}
-
 export type IndexDescription<A> = MongoIndexDescription & {
   readonly key: {
     readonly [B in keyof A]?: 1 | -1 | 'text'
