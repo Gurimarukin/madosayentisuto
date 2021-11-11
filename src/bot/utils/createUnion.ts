7adf8894cb6bda4406cd0171310fb42f046d4831
Gurimarukin <naxel@laposte.net>
2021-11-11 06:29:43 +0100

    Refacto

--- src/bot/utils/createUnion.ts
+++ src/bot/utils/createUnion.ts
@@ -1,47 +1,34 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-
-/* eslint-disable functional/immutable-data */
+/* eslint-disable @typescript-eslint/explicit-function-return-type,
+                  @typescript-eslint/no-explicit-any,
+                  @typescript-eslint/strict-boolean-expressions,
+                  functional/immutable-data,
+                  functional/no-expression-statement */
 
 /**
  * source: https://github.com/AlexGalays/spacelift/blob/be302c4807b23114de27dda6a90b315c3af56631/src/union.ts
  */
 import type { List } from '../../shared/utils/fp'
 
-/* eslint-disable functional/no-expression-statement */
 type UnionDescription = Record<string, (...args: List<any>) => any>
 
 type UnionResult<T extends UnionDescription> = {
   readonly T: Union<T>
   readonly is: <NAME extends keyof T>(
     name: NAME,
-  ) => <U extends Union<T>>(other: U) => other is MyReturnType<T[NAME]> & { readonly type: NAME }
-} & { readonly [K in keyof T]: Factory<T[K], K> & { readonly T: MyReturnType<Factory<T[K], K>> } }
-
-// Same as the std lib's ReturnType but without the constraint on T as the compiler can't check it against Arguments<F>
-type MyReturnType<T> = T extends (...args: List<any>) => infer R ? R : any
+  ) => <U extends Union<T>>(other: U) => other is ReturnType<T[NAME]> & { readonly type: NAME }
+} & { readonly [K in keyof T]: Factory<T[K], K> & { readonly T: ReturnType<Factory<T[K], K>> } }
 
 type Factory<F extends (...args: List<any>) => any, TYPE> = (
-  ...args: Arguments<F>
+  ...args: Parameters<F>
 ) => F extends (...args_: List<any>) => infer R
   ? { readonly [K in keyof R | 'type']: K extends 'type' ? TYPE : R[K & keyof R] }
   : (...args_: List<any>) => any
 
 type Union<T extends UnionDescription> = {
   readonly [K in keyof T]: {
-    readonly [K2 in keyof MyReturnType<T[K]> | 'type']: K2 extends 'type'
-      ? K
-      : MyReturnType<T[K]>[K2]
+    readonly [K2 in keyof ReturnType<T[K]> | 'type']: K2 extends 'type' ? K : ReturnType<T[K]>[K2]
   }
 }[keyof T]
-
-// Note: This has a small limitation in tooltips: https://github.com/Microsoft/TypeScript/issues/28127
-type Arguments<T extends (...args: List<any>) => any> = T extends (...args: infer A) => any
-  ? A
-  : readonly []
 
 /**
  * Creates a type-safe union, providing: derived types, factories and type-guards in a single declaration.
