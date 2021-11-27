f897b8840d4256cfdce6c319cd52e9a9b3f49ead
Gurimarukin <naxel@laposte.net>
2021-11-27 17:49:19 +0100

    Add update

--- src/bot/models/Store.ts
+++ src/bot/models/Store.ts
@@ -1,19 +1,26 @@
+import type { Endomorphism } from 'fp-ts/Endomorphism'
+
 import { IO } from '../../shared/utils/fp'
 
 export type Store<A> = {
   readonly get: IO<A>
   readonly set: (a: A) => IO<void>
+  readonly update: (f: Endomorphism<A>) => IO<void>
 }
 
 export const Store = <A>(init: A): Store<A> => {
   // eslint-disable-next-line functional/no-let
   let value = init
+
+  const update = (f: Endomorphism<A>): IO<void> =>
+    IO.fromIO(() => {
+      // eslint-disable-next-line functional/no-expression-statement
+      value = f(value)
+    })
+
   return {
     get: IO.fromIO(() => value),
-    set: (a: A) =>
-      IO.fromIO(() => {
-        // eslint-disable-next-line functional/no-expression-statement
-        value = a
-      }),
+    set: (a: A): IO<void> => update(() => a),
+    update,
   }
 }
