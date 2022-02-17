4e5d89469f55aec3d109ad37febbd9ed2c998dde
Gurimarukin <naxel@laposte.net>
2022-02-17 21:18:58 +0100

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,12 @@ module.exports = {
   },
   plugins: ['functional', 'fp-ts'],
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
     'plugin:functional/recommended',
     'plugin:fp-ts/all',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier',
   ],
   settings: {
     react: {
