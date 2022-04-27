f119620589b3667b2ae26e2589acd39af152f1b1
Gurimarukin <naxel@laposte.net>
2022-04-27 17:12:52 +0200

    Fix z-index

--- src/client/components/Tooltip.tsx
+++ src/client/components/Tooltip.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const Tooltip: React.FC<Props> = ({ title, className, children }) => (
   <div className={cssClasses('relative group', className)}>
     {children}
-    <div className="absolute left-1/2 top-[calc(100%_+_2px)] opacity-0 blur invisible duration-300 group-hover:opacity-100 group-hover:blur-0 group-hover:visible">
+    <div className="absolute z-50 left-1/2 top-[calc(100%_+_2px)] opacity-0 blur invisible duration-300 group-hover:opacity-100 group-hover:blur-0 group-hover:visible">
       <div className="relative left-[-50%] flex px-3 py-2 bg-gray1 rounded">{title}</div>
     </div>
   </div>
