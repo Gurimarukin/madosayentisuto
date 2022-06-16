8dbff7b64298d37443e00bfa7e896d266bc9a618
Gurimarukin <naxel@laposte.net>
2022-06-16 11:35:01 +0200

    Horizontal dnd

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,16 +3,20 @@ import React from 'react'
 import { HistoryContextProvider } from './contexts/HistoryContext'
 import { HttpContextProvider } from './contexts/HttpContext'
 import { LogContextProvider } from './contexts/LogContext'
+import { HTML5Backend } from './libs/backend-html5'
+import { DndProvider } from './libs/react-dnd'
 import { AppRouterComponent } from './router/AppRouterComponent'
 
 export const App = (): JSX.Element => (
-  <HistoryContextProvider>
-    <HttpContextProvider>
-      <LogContextProvider>
-        <div className="overflow-hidden w-[100vw] h-[100vh] font-[baloopaaji2] text-gray4 bg-gray3">
-          <AppRouterComponent />
-        </div>
-      </LogContextProvider>
-    </HttpContextProvider>
-  </HistoryContextProvider>
+  <div className="overflow-hidden w-[100vw] h-[100vh] font-[baloopaaji2] text-gray4 bg-gray3">
+    <HistoryContextProvider>
+      <HttpContextProvider>
+        <LogContextProvider>
+          <DndProvider backend={HTML5Backend}>
+            <AppRouterComponent />
+          </DndProvider>
+        </LogContextProvider>
+      </HttpContextProvider>
+    </HistoryContextProvider>
+  </div>
 )
--- src/client/domain/guild/emojis/GuildEmojis.tsx
+++ src/client/domain/guild/emojis/GuildEmojis.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable functional/no-expression-statement */
 import { pipe } from 'fp-ts/function'
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 
-import { GuildEmojiId } from '../../../../shared/models/guild/GuildEmojiId'
+import type { GuildEmojiView } from '../../../../shared/models/guild/GuildEmojiView'
 import type { GuildId } from '../../../../shared/models/guild/GuildId'
-import { Maybe } from '../../../../shared/utils/fp'
+import { List, Maybe } from '../../../../shared/utils/fp'
 
 import { Tooltip } from '../../../components/Tooltip'
+import type { Identifier } from '../../../libs/dnd-core'
+import type { XYCoord } from '../../../libs/react-dnd'
+import { useDrag, useDrop } from '../../../libs/react-dnd'
 import { GuildLayout } from '../GuildLayout'
 
 type Props = {
@@ -14,31 +18,151 @@ type Props = {
 
 export const GuildEmojis = ({ guildId }: Props): JSX.Element => (
   <GuildLayout guildId={guildId} selected="emojis">
-    {guild => (
-      <ul className="flex flex-wrap gap-6 justify-center content-start p-6 w-full">
-        {guild.emojis.map(emoji => (
-          <li key={GuildEmojiId.unwrap(emoji.id)}>
-            <Tooltip
-              title={pipe(
-                emoji.name,
-                Maybe.getOrElse(() => emoji.url),
-              )}
-            >
-              <img
-                src={emoji.url}
-                alt={pipe(
-                  emoji.name,
-                  Maybe.fold(
-                    () => 'Emoji inconnu',
-                    n => `Emoji ${n}`,
-                  ),
-                )}
-                className="object-contain w-28 h-28 border border-gray1"
-              />
-            </Tooltip>
-          </li>
-        ))}
-      </ul>
-    )}
+    {guild => <Container emojis={guild.emojis} />}
   </GuildLayout>
 )
+
+type ContainerProps = {
+  readonly emojis: List<GuildEmojiView>
+}
+
+type Item = {
+  readonly id: number
+  readonly emoji: GuildEmojiView
+}
+
+const Container = ({ emojis }: ContainerProps): JSX.Element => {
+  const [cards, setCards] = useState<List<Item>>(() =>
+    pipe(
+      emojis,
+      List.mapWithIndex((id, emoji) => ({ id, emoji })),
+    ),
+  )
+
+  const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
+    setCards(prevCards =>
+      pipe(
+        prevCards,
+        List.deleteAt(dragIndex),
+        Maybe.getOrElse(() => prevCards),
+        deleted =>
+          pipe(
+            List.lookup(dragIndex, prevCards),
+            Maybe.chain(dragged => pipe(deleted, List.insertAt(hoverIndex, dragged))),
+          ),
+        Maybe.getOrElse(() => prevCards),
+      ),
+    )
+  }, [])
+
+  const renderCard = useCallback(
+    (card: Item, index: number) => (
+      <GuildEmoji key={card.id} index={index} id={card.id} emoji={card.emoji} moveCard={moveCard} />
+    ),
+    [moveCard],
+  )
+
+  return (
+    <div className="flex flex-wrap gap-6 justify-center content-start p-6 w-full">
+      {cards.map((card, i) => renderCard(card, i))}
+    </div>
+  )
+}
+
+type CardProps = {
+  readonly id: number
+  readonly emoji: GuildEmojiView
+  readonly index: number
+  // eslint-disable-next-line functional/no-return-void
+  readonly moveCard: (dragIndex: number, hoverIndex: number) => void
+}
+
+type DragItem = {
+  // eslint-disable-next-line functional/prefer-readonly-type
+  index: number
+  readonly id: string
+}
+
+const emojiType = 'card' as const
+
+const GuildEmoji = ({ id, emoji, index, moveCard }: CardProps): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [{ handlerId }, drop] = useDrop<DragItem, void, { readonly handlerId: Identifier | null }>({
+    accept: emojiType,
+    collect: monitor => ({ handlerId: monitor.getHandlerId() }),
+    hover: (item: DragItem, monitor) => {
+      if (ref.current === null) return
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) return
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current.getBoundingClientRect()
+
+      // Get horizontal center
+      const hoverCenterX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+
+      // Get pixels to the left
+      const hoverClientX = (clientOffset as XYCoord).x - hoverBoundingRect.left
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientX < hoverCenterX) return
+
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientX > hoverCenterX) return
+
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex)
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      // eslint-disable-next-line functional/immutable-data
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: emojiType,
+    item: () => ({ id, index }),
+    collect: monitor => ({ isDragging: monitor.isDragging() }),
+  })
+
+  const opacity = isDragging ? 0 : 1
+
+  drag(drop(ref))
+
+  return (
+    <div ref={ref} data-handler-id={handlerId}>
+      <Tooltip
+        title={pipe(
+          emoji.name,
+          Maybe.getOrElse(() => emoji.url),
+        )}
+      >
+        <img
+          src={emoji.url}
+          alt={pipe(
+            emoji.name,
+            Maybe.fold(
+              () => 'Emoji inconnu',
+              n => `Emoji ${n}`,
+            ),
+          )}
+          className="object-contain w-28 h-28 border border-gray1"
+          style={{ opacity }}
+        />
+      </Tooltip>
+    </div>
+  )
+}
