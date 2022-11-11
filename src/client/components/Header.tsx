1db88d48f90507376ef8364c585ea302c10069d3
Gurimarukin <naxel@laposte.net>
2022-11-11 13:30:11 +0100

    styling

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -6,7 +6,7 @@ import { Link } from './Link'
 export const Header: React.FC = ({ children }) => (
   <header className="flex items-center border-b border-gray1 bg-gray2 pr-5 shadow-lg">
     <div className="flex grow items-center gap-x-4 p-2">{children}</div>
-    <Link to={appRoutes.index} className="text-2xl">
+    <Link to={appRoutes.index} className="py-4 text-2xl">
       Accueil
     </Link>
   </header>
