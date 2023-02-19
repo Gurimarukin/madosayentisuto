6f7d38c7b0603a8e84a56fb8f0ae4005c153902d
Gurimarukin <naxel@laposte.net>
2023-02-19 12:55:04 +0100

    fix WithIp

--- src/server/webServer/utils/WithIp.ts
+++ src/server/webServer/utils/WithIp.ts
@@ -7,8 +7,8 @@ import { Maybe, Tuple } from '../../../shared/utils/fp'
 
 import type { Config } from '../../config/Config'
 import type { LoggerGetter } from '../../models/logger/LoggerObservable'
-import { MyMiddleware as M } from '../models/MyMiddleware'
 import type { EndedMiddleware } from '../models/MyMiddleware'
+import { MyMiddleware as M } from '../models/MyMiddleware'
 
 export type WithIp = (cause: string) => (f: (ip: string) => EndedMiddleware) => EndedMiddleware
 
@@ -32,23 +32,24 @@ export const WithIp = (Logger: LoggerGetter, config: Config): WithIp => {
         remoteAddress: M.decodeHeader('remote-address', maybeStr),
         xRealIp: M.decodeHeader('x-real-ip', maybeStr),
       }),
-      M.ichain(({ xForwardedFor, remoteAddress, xRealIp }) =>
-        pipe(
-          xForwardedFor,
-          Maybe.alt(() => remoteAddress),
-          Maybe.alt(() => xRealIp),
-          Maybe.alt(() => (config.isDev ? Maybe.some('127.0.0.1') : Maybe.none)),
-          Maybe.fold(
-            () =>
-              pipe(
-                logger.error(`Request rejected because ip is required for ${cause}`),
-                M.fromIOEither,
-                M.ichain(() => M.sendWithStatus(Status.BadRequest)('')),
-              ),
-            ip => f(ip),
+      M.matchE(
+        () => M.sendWithStatus(Status.BadRequest)(''),
+        ({ xForwardedFor, remoteAddress, xRealIp }) =>
+          pipe(
+            xForwardedFor,
+            Maybe.alt(() => remoteAddress),
+            Maybe.alt(() => xRealIp),
+            Maybe.alt(() => (config.isDev ? Maybe.some('127.0.0.1') : Maybe.none)),
+            Maybe.fold(
+              () =>
+                pipe(
+                  logger.error(`Request rejected because ip is required for ${cause}`),
+                  M.fromIOEither,
+                  M.ichain(() => M.sendWithStatus(Status.BadRequest)('')),
+                ),
+              ip => f(ip),
+            ),
           ),
-        ),
       ),
-      M.orElse(() => M.sendWithStatus(Status.BadRequest)('')),
     )
 }
