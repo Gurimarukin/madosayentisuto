644cc8296be44c29b160533b1a7f8334c56e8628
Gurimarukin <naxel@laposte.net>
2024-02-11 12:51:36 +0100

    log

--- src/shared/utils/config/loadDotEnv.ts
+++ src/shared/utils/config/loadDotEnv.ts
@@ -4,6 +4,8 @@ import { pipe } from 'fp-ts/function'
 import type { Dict } from '../fp'
 import { IO } from '../fp'
 
+console.log('process.env =', process.env)
+
 export const loadDotEnv: IO<Dict<string, string | undefined>> = pipe(
   IO.tryCatch(() => dotenv.config()),
   IO.chain(result =>
