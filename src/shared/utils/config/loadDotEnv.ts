1885e74bcd6f23b9f2d424430e549397516dae81
Gurimarukin <naxel@laposte.net>
2024-02-11 12:24:19 +0100

    fix loadDotEnv

--- src/shared/utils/config/loadDotEnv.ts
+++ src/shared/utils/config/loadDotEnv.ts
@@ -4,11 +4,11 @@ import { pipe } from 'fp-ts/function'
 import type { Dict } from '../fp'
 import { IO } from '../fp'
 
-export const loadDotEnv: IO<Dict<string, string>> = pipe(
+export const loadDotEnv: IO<Dict<string, string | undefined>> = pipe(
   IO.tryCatch(() => dotenv.config()),
   IO.chain(result =>
     result.parsed !== undefined
-      ? IO.right(result.parsed)
+      ? IO.right(process.env)
       : result.error !== undefined
         ? IO.left(result.error)
         : IO.left(Error('result.error was undefined')),
