8ff59bc8dd40d52ca6313352ebadc3bc20a706a7
Gurimarukin <naxel@laposte.net>
2026-01-08 22:02:46 +0100

    bump node, bcrypt and discord voice

--- src/server/utils/PasswordUtils.ts
+++ src/server/utils/PasswordUtils.ts
@@ -1,24 +1,22 @@
-import argon2 from '@phc/argon2'
+import bcrypt from 'bcrypt'
 import { pipe } from 'fp-ts/function'
-import upash from 'upash'
 
 import { ClearPassword } from '../../shared/models/webUser/ClearPassword'
 import { Future } from '../../shared/utils/fp'
 
 import { HashedPassword } from '../models/webUser/HashedPassword'
 
-// eslint-disable-next-line functional/no-expression-statements
-upash.install('argon2', argon2)
+const saltRounds = 10
 
 const hash = (clearPassword: ClearPassword): Future<HashedPassword> =>
   pipe(
-    Future.tryCatch(() => upash.hash(ClearPassword.unwrap(clearPassword))),
+    Future.tryCatch(() => bcrypt.hash(ClearPassword.unwrap(clearPassword), saltRounds)),
     Future.map(HashedPassword.wrap),
   )
 
 const check = (hashedPassword: HashedPassword, clearPassword: ClearPassword): Future<boolean> =>
   Future.tryCatch(() =>
-    upash.verify(HashedPassword.unwrap(hashedPassword), ClearPassword.unwrap(clearPassword)),
+    bcrypt.compare(HashedPassword.unwrap(hashedPassword), ClearPassword.unwrap(clearPassword)),
   )
 
 export const PasswordUtils = { hash, check }
